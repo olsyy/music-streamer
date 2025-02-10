3c129546f12a0496b491efb29d0f39468546e3bf
Denethof <denethof@student.21-school.ru>
2025-02-10 15:35:38 +0300

    Init project

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MusicStreamer"
+include(":app")
+include(":core")
+include(":features")
+include(":features:downloaded-tracks")
+include(":features:api-tracks")
+include(":features:playback")
+ 
